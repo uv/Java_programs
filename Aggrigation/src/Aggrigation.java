9697d0ef1414dda3590a215e062597f6c1ad9a8d
uv <yuvraj.akhade399@gmail.com>
2017-08-03 12:53:31 +0530

    Aggregation Program

---
+++ Aggrigation/src/Aggrigation.java
@@ -0,0 +1,25 @@
+
+class Operation{
+	 int square(int n){
+	  return n*n;
+	 }
+	}
+
+	class Aggrigation{
+	 Operation op;//aggregation
+	 double pi=3.14;
+	  
+	 double area(int radius){
+	   op=new Operation();
+	   int rsquare=op.square(radius);//code reusability 
+	   return pi*rsquare;
+	 }
+
+	   
+	  
+	 public static void main(String args[]){
+		 Aggrigation c=new Aggrigation();
+	   double result=c.area(2);
+	   System.out.println("Area of Circle = "+result);
+	 }
+	}
