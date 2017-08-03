8bff9ca0307efc720ecc183f4c1b753b45a9cda7
uv <yuvraj.akhade399@gmail.com>
2017-08-03 11:53:54 +0530

    Aggregation(Area of circle)program

--- ExceptionHandling/src/Aggrigation.java
+++ ExceptionHandling/src/Aggrigation.java
@@ -1,9 +1,24 @@
-
-public class Aggrigation {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+class Operation{
+	 int square(int n){
+	  return n*n;
+	 }
 	}
 
-}
+	class Aggrigation{
+	 Operation op;//aggregation
+	 double pi=3.14;
+	  
+	 double area(int radius){
+	   op=new Operation();
+	   int rsquare=op.square(radius);//code reusability (i.e. delegates the method call).
+	   return pi*rsquare;
+	 }
+
+	   
+	  
+	 public static void main(String args[]){
+		 Aggrigation c=new Aggrigation();
+	   double result=c.area(2);
+	   System.out.println("Area of Circle = "+result);
+	 }
+	}
