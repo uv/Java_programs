c72ba2b95d71094c9a1e37a3e5318a0b9cce1d5a
uv <yuvraj.akhade399@gmail.com>
2017-08-18 13:53:39 +0530

    Thread Program Using Synchronization

--- MultiThreading/src/com/thread/ThreadExample.java
+++ MultiThreading/src/com/thread/ThreadExample.java
@@ -1,5 +1,48 @@
 package com.thread;
 
-public class ThreadExample {
-
+public class ThreadExample extends Thread {
+	public synchronized void  table(int i)
+	{
+		for(int j=1;j<11;j++)
+		{
+			System.out.println(i*j);
+			try {
+				currentThread();
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	public static void main(String[] args) {
+		ThreadExample te=new ThreadExample();
+		Thread1 t=new Thread1(te);
+		Thread2 t1=new Thread2(te);
+		t.start();
+		t1.start();
+	}
 }
+ class Thread1 extends Thread{
+	 ThreadExample te;
+		public Thread1(ThreadExample te)
+		{
+			this.te=te;
+		}
+		public void run()
+		{
+			te.table(10);
+		}
+	}
+
+ class Thread2 extends Thread{
+	 ThreadExample te;
+		public Thread2(ThreadExample te)
+		{
+			this.te=te;
+		}
+		public void run()
+		{
+			te.table(20);
+		}
+	}
